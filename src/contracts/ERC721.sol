871a2030dc211dca9e3f120fa58bc8db0b6a027d
cvaldrighi <valdrighdev@gmail.com>
2022-09-21 17:07:27 -0300

    nft contracts

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ERC721 {
+    mapping(uint256 => address) private _tokenOwner;
+    mapping(address => uint256) private _OwnedTokensCount;
+
+    // constructor() {}
+
+    // function _mint(address to, uint tokenId) internal view {
+
+    // }
+}
