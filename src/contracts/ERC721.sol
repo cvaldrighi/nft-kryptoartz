7c195a886cc1fdbe895748475a62c7dc34e11778
cvaldrighi <valdrighdev@gmail.com>
2022-09-24 17:44:20 -0300

    transferFrom function

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -9,11 +9,12 @@ contract ERC721 {
     );
 
     mapping(uint256 => address) private _tokenOwner;
-    mapping(address => uint256) private _OwnedTokensCount;
+    mapping(address => uint256) private _ownedTokensCount;
+    mapping(uint256 => address) private _tokenApprovals;
 
     function balanceOf(address _owner) public view returns (uint256) {
         require(_owner != address(0), "Owner query for non-existent token");
-        return _OwnedTokensCount[_owner];
+        return _ownedTokensCount[_owner];
     }
 
     function ownerOf(uint256 _tokenId) public view returns (address) {
@@ -34,8 +35,34 @@ contract ERC721 {
         require(!_exists(tokenId), "ERC721: token already minted");
 
         _tokenOwner[tokenId] = to;
-        _OwnedTokensCount[to] += 1;
+        _ownedTokensCount[to] += 1;
 
         emit Transfer(address(0), to, tokenId);
+    }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal {
+        require(_to != address(0), "ERC721: Transfer to the zero address");
+        require(
+            ownerOf(_tokenId) == _from,
+            "ERC721: Trying to transfer a token that the address does not own"
+        );
+
+        _tokenOwner[_tokenId] = _to;
+        _ownedTokensCount[_from] -= 1;
+        _ownedTokensCount[_to] += 1;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        _transferFrom(_from, _to, _tokenId);
     }
 }
