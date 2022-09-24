0e36d9a94d65687378f68080eb12a9b5b4502989
cvaldrighi <valdrighdev@gmail.com>
2022-09-23 22:57:04 -0300

    ERC721Enumerable helper functions

---
+++ src/contracts/ERC721Enumerable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721.sol";
+
+contract ERC721Enumerable is ERC721 {
+    uint256[] private _allTokens;
+    mapping(uint256 => uint256) private _allTokensIndex;
+    mapping(address => uint256[]) private _ownedTokens;
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+
+    //return the total supply of _allTokens array
+    function totalSupply() public view returns (uint256) {
+        return _allTokens.length;
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "Global index is out of bounds.");
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        returns (uint256)
+    {
+        require(index < balanceOf(owner), "Owner index is out of bounds.");
+        return _ownedTokens[owner][index];
+    }
+
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    function _addTokensToOwnerEnumaration(address to, uint256 tokenId) private {
+        _ownedTokens[to].push(tokenId);
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+    }
+
+    function _mint(address to, uint256 tokenId) internal override(ERC721) {
+        super._mint(to, tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumaration(to, tokenId);
+    }
+}
